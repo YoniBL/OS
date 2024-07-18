25b7f6b2959dee0c0610503eacb5a8a503e3868a
Yonatan Benizri Levi <144797247+YoniBL@users.noreply.github.com>
2024-07-18 14:15:29 +0300

    Update message_sender.c

--- message_sender.c
+++ message_sender.c
@@ -19,7 +19,7 @@ int main (int argc, char **argv){
     
     if (argc != 4){
         fprintf(stderr,"%s\n", strerror(EINVAL));
-        return 1;
+        exit(1);
     }
 
     fd = open(argv[1],O_RDWR);
@@ -28,12 +28,12 @@ int main (int argc, char **argv){
     ch_id = strtol(argv[2],NULL,10);
     if (ch_id <= 0){
         fprintf(stderr,"%s\n", strerror(EINVAL));
-        return 1;
+        exit(1);
     }
 
     if ((ioctl(fd,MSG_SLOT_CHANNEL, ch_id)<0)||(write(fd,argv[3],strlen(argv[3]))<0) || (close(fd)<0)) print_and_exit();
 
-    return 0;
+    exit(0);
 }
 
 
